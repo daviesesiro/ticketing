245b8acc8b3d1d006031875bc9e53b8257b0f5b2
Ojurereoluwa Davies <daviesesiro@gmail.com>
2022-02-21 03:25:54 +0100

    updated client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,20 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const bulidClient = ({ req }) => {
-  if (typeof window === "undefined") {
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: 'http://www.ticketing-app-prod.xyz',
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: "/",
+      baseUrl: '/',
     });
   }
 };
-
-export default bulidClient;
--- client/components/header.js
+++ client/components/header.js
@@ -1,10 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-const Header = ({ currentUser }) => {
+export default ({ currentUser }) => {
   const links = [
-    !currentUser && { label: "Sign Up", href: "/auth/signup" },
-    !currentUser && { label: "Sign In", href: "/auth/signin" },
-    currentUser && { label: "Sign Out", href: "/auth/signout" },
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+    currentUser && { label: 'My Orders', href: '/orders' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
@@ -29,5 +31,3 @@ const Header = ({ currentUser }) => {
     </nav>
   );
 };
-
-export default Header;
